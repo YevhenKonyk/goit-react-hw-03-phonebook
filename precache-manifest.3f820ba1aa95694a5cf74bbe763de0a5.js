7bef951624b5c0f79205cf6d2b6cdf44bb60f550
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-01-09 16:54:20 +0200

    Updates

---
+++ precache-manifest.3f820ba1aa95694a5cf74bbe763de0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e25245c80264e570d02f13030e229",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "a8ce166a4ca8e9ed6da4",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.6bfce584.chunk.css"
+  },
+  {
+    "revision": "e9d670e217b627672cac",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.d4d75a8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.d4d75a8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8ce166a4ca8e9ed6da4",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.033433bd.chunk.js"
+  },
+  {
+    "revision": "30270bd832f8a1815aaf",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.37614506.js"
+  }
+]);
